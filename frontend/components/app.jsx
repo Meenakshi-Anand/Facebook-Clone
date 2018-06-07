3c62f3179e785150e2220070b94db3edcda7c579
Meenakshi Anand <abhi@codeanand.com>
2018-06-07 16:33:21 -0700

    Fix  redirect issue

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route,Redirect,Switch} from 'react-router-dom';
 import Header from './navbar/header_container';
 import Greeting from './navbar/greeting';
 import MainPage from './mainpage/mainpage_container';
@@ -8,7 +8,10 @@ import LoginPage from './pages/login_page';
 import { AuthRoute } from '../util/route_util';
 const App = () => (
   <div>
-    <Route path="/" component={LoginPage} />
+    <Switch>
+    <Route exact path="/" component={LoginPage} />
+    <Redirect to="/" />
+    </Switch>s
   </div>
 );
 export default App;
