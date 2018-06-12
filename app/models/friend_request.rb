8a12010ed8b3698c58d8ab04d7b235aae4fe22cd
Meenakshi Anand <abhi@codeanand.com>
2018-06-11 17:01:37 -0700

    changed class name in friend request

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -5,11 +5,11 @@ validates :approval_status,inclusion: { in: [true, false] }
 belongs_to :requestor,
 primary_key: :id,
 foreign_key: :requestor_id,
-className: :User
+class_name: :User
 
 belongs_to :approver,
 primary_key: :id,
 foreign_key: :approver_id,
-className: :User
+class_name: :User
 
 end
