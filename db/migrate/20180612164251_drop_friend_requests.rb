a6f603744f16e86a4b42ece20b36e1c93eaa7d6f
Meenakshi Anand <abhi@codeanand.com>
2018-06-15 18:02:47 -0700

    fix issues in migration

--- db/migrate/20180612164251_drop_friend_requests.rb
+++
@@ -1,5 +0,0 @@
-class DropFriendRequests < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :friend_requests
-  end
-end
--- frontend/components/posts/news_feed_index.jsx
+++ frontend/components/posts/news_feed_index.jsx
@@ -5,12 +5,6 @@ import CreatePostFormContainer from './create_post_form_container';
 class NewsFeedIndex extends React.Component {
   constructor(props){
     super(props);
-    this.state={loading:true};
-  }
-  componentDidMount() {
-    this.props.fetchNewsFeed()
-    .then(()=>this.setState({
-      loading:false}));
   }
 
   render() {
