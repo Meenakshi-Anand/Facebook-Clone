095d177299e34b03af953017f39c2e3087a17b09
Meenakshi-Anand <abhi@codeanand.com>
2018-07-20 19:09:56 -0700

    Error rendering for sign up container

--- frontend/components/mainpage/signup_form.jsx
+++ frontend/components/mainpage/signup_form.jsx
@@ -11,7 +11,16 @@ class SignupForm extends React.Component {
       fname: "",
       lname: "",
       birthday: "",
-      sex: ""
+      sex: "",
+
+    };
+    this.err = {
+      email_error:"",
+      password_error:"",
+      fname_error:"",
+      lname_error:"",
+      birthday_error:"",
+      sex_error:""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -38,16 +47,30 @@ class SignupForm extends React.Component {
 
   renderErrors(){
     const {errors} = this.props;
-    // const { loginErrors } = errors.login_errors;
-    return (<ul >
-      {errors.signupErrors.map((error,i) => (
-            <li key={`error-${i}`}>
-              {error}
-            </li>
-          ))}
-       </ul>);
+    errors.signupErrors.forEach((error) => {
+      if (error === "Email can't be blank"){
+         this.err.email_error = "You'll use this when you log in and if you ever need to reset your password.";
+      }else if(error === "Fname can't be blank"){
+         this.err.fname_error = "Whats your name?";
+      }else if(error === "Lname can't be blank"){
+         this.err.lname_error ="Whats your name?";
+      }else if(error === "Birthday can't be blank"){
+         this.err.birthday_error =
+        "Select your birthday. You can change who can see this later.";
+      }else if(error === "Password is too short (minimum is 6 characters)"){
+         this.err.password_error =
+        "Enter a combination of at least six numbers, letters and punctuation marks (like ! and &).";
+      }
+    });
   }
-
+  renderSmallErrors(variable){
+    console.log(variable);
+    if (variable){
+      return(<span className="tooltiptext">{variable}</span>);
+    }else {
+      return(null);
+    }
+  }
   renderSignup(){
     const {formType} = this.props;
     if (formType === "SIGNUP"){
@@ -59,28 +82,49 @@ class SignupForm extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <div>
           <label>
-            <input type="text" placeholder={'First Name'}
+            <section className="tooltip">
+            <input type="text"
+              className={ this.err.fname_error === "" ? '' : 'error'}
+              placeholder={'First Name'}
               value={this.state.fname} onChange={this.update('fname')} />
+            {this.renderSmallErrors(this.err.fname_error)}
+          </section>
           </label>
           <label>
+            <section className="tooltip">
             <input type="text" placeholder={'Last Name'}
+              className={ this.err.lname_error === "" ? '' : 'error'}
               value={this.state.lname} onChange={this.update('lname')} />
+            {this.renderSmallErrors(this.err.lname_error)}
+            </section>
          </label>
         </div>
           <label>
+          <section className="tooltip">
           <input type="text" placeholder={'Email'}
+            className={ this.err.email_error === "" ? '' : 'error'}
             value={this.state.email} onChange={this.update('email')} />
+          {this.renderSmallErrors(this.err.email_error)}
+            </section>
           </label>
           <label>
+            <section className="tooltip">
             <input type="text" placeholder={'New Password'}
+              className={ this.err.password_error === "" ? '' : 'error'}
               value={this.state.password} onChange={this.update('password')} />
+            {this.renderSmallErrors(this.err.password_error)}
+                </section>
           </label>
           <div className="birthday-signup">
 
           <label> <h3 className="bsub-text"> Birthday </h3>
             <div>
+            <section className="tooltip">
             <input type="date" value={this.state.birthday}
+              className={ this.err.birthday_error === "" ? '' : 'error'}
               onChange={this.update('birthday')} />
+            {this.renderSmallErrors(this.err.birthday_error)}
+                  </section>
             <span className="plink margintop">Why do I need to provide
               my birthday?</span>
             </div>
